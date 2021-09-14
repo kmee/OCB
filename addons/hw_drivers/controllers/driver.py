aab7c9591913d04bf2e12781fbb965ac730e33bf
Luis Felipe Mileo <mileo@kmee.com.br>
2021-09-14 12:29:36 -0300

    [REM] Displays

--- addons/hw_drivers/controllers/driver.py
+++ addons/hw_drivers/controllers/driver.py
@@ -385,21 +385,21 @@ class Manager(Thread):
     def get_connected_displays(self):
         display_devices = {}
 
-        displays = subprocess.check_output(['tvservice', '-l']).decode()
-        x_screen = 0
-        for match in finditer('Display Number (\d), type HDMI (\d)', displays):
-            display_id, hdmi_id = match.groups()
-            tvservice_output = subprocess.check_output(['tvservice', '-nv', display_id]).decode().rstrip()
-            if tvservice_output:
-                display_name = tvservice_output.split('=')[1]
-                display_identifier = sub('[^a-zA-Z0-9 ]+', '', display_name).replace(' ', '_') + "_" + str(hdmi_id)
-                iot_device = IoTDevice({
-                    'identifier': display_identifier,
-                    'name': display_name,
-                    'x_screen': str(x_screen),
-                }, 'display')
-                display_devices[display_identifier] = iot_device
-                x_screen += 1
+        # displays = subprocess.check_output(['tvservice', '-l']).decode()
+        # x_screen = 0
+        # for match in finditer('Display Number (\d), type HDMI (\d)', displays):
+        #     display_id, hdmi_id = match.groups()
+        #     tvservice_output = subprocess.check_output(['tvservice', '-nv', display_id]).decode().rstrip()
+        #     if tvservice_output:
+        #         display_name = tvservice_output.split('=')[1]
+        #         display_identifier = sub('[^a-zA-Z0-9 ]+', '', display_name).replace(' ', '_') + "_" + str(hdmi_id)
+        #         iot_device = IoTDevice({
+        #             'identifier': display_identifier,
+        #             'name': display_name,
+        #             'x_screen': str(x_screen),
+        #         }, 'display')
+        #         display_devices[display_identifier] = iot_device
+        #         x_screen += 1
 
         if not len(display_devices):
             # No display connected, create "fake" device to be accessed from another computer
